a1b6cd2651014d14296b3a4de909efad241850ab
Mateo Menestrina <mateo.menestrina@gmail.com>
2018-11-06 12:06:47 -0300

    wip

--- src/components/Network.js
+++ src/components/Network.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import {Metrica} from './Utils';
-<<<<<<< HEAD
 
-=======
-import PostContainer from './PostContainer'
->>>>>>> capacitacion-completa
 
 class Network extends Component {
 
@@ -35,9 +31,7 @@ class Network extends Component {
                   { this.props.data.stats.map( (metric, index) => <Metrica name={metric.metric}  value ={metric.value}  key={index}   /> ) } 
               </div>
 
-              <PostContainer posts={this.props.data.posts} />
-
-             
+        
           </div>
         </div>
      
